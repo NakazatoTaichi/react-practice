4810001e7d3d481643d3f356f9b452b537a16822
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2025-06-04 15:46:02 +0900

    一旦コミット

--- react-tutorial-01/src/App.js
+++ react-tutorial-01/src/App.js
@@ -1,8 +1,18 @@
-import './App.css';
+import { useState } from 'react';
+import TodoList from './TodoList';
 
 function App() {
+  const [todos,] = useState([
+    { id: 1, name: "Todo1", completed: false },
+  ]);
   return (
-    <div>Hello React</div>
+    <div>
+      <TodoList todos={todos} />
+      <input type="text" />
+      <button>タスクを追加</button>
+      <button>完了したタスクの削除</button>
+      <div>残りのタスク：0</div>
+    </div>
   );
 }
 
